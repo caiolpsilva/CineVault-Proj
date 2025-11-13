5f0b79ca2bce9d4f6edc2e9d492809573fe34791
caiosilva20 <caiorumoaodev@gmail.com>
2025-11-13 12:11:24 -0300

    ready

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true
 })
 export class PipeTruncar implements PipeTransform {
-  transform(valor: string, limite: number = 100): string {
+  transform(valor: string, limite: number = 25): string {
     if (!valor) return '';
     return valor.length > limite ? valor.substring(0, limite) + '...' : valor;
   }
